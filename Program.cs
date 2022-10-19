f2ef245a4fdc0fa0c5958d9390a03b1474ae228e
矢部巧磨 <@MSI>
2022-10-19 15:25:22 +0900

    GOTbranchに変更

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello,GOT !");
+Console.WriteLine("Hello,GOTburanch !");
 
 Console.WriteLine("Hello,MasterBranch");
