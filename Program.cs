2d5ee7b32967ac7d16f0862cd7f713673d970f1b
矢部巧磨 <@MSI>
2022-10-19 15:24:02 +0900

    すべての行をMasuterBuranchに変更

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello,GOT !");
+Console.WriteLine("Hello,Masterbranch !");
 
-Console.WriteLine("Hello,MasterBranch");
+Console.WriteLine("Hello,Masterbranch");
